d11977d2c1c880133db9b37a028171956b395481
LishaDeng <lishadeng@hotmail.com>
2016-11-15 00:39:24 -0500

    Login done

--- WorkoutEveryday.js
+++ WorkoutEveryday.js
@@ -6,3 +6,25 @@ function limit(element)
         element.value = element.value.substr(0, max_chars);
     }
 }
+/*限制4位数字输入*/
+
+$.fn.center = function () {
+  this.css("position","absolute");
+  this.css("top", Math.max(0, (
+    ($(window).height() - $(this).outerHeight()) / 2) + 
+     $(window).scrollTop()) + "px"
+  );
+  this.css("left", Math.max(0, (
+    ($(window).width() - $(this).outerWidth()) / 2) + 
+     $(window).scrollLeft()) + "px"
+  );
+  return this;
+}
+
+$("#overlay").show();
+$("#overlay-content").show().center();
+
+setTimeout(function(){    
+  $("#overlay").fadeOut();
+}, 5000);
+/*开机画面停留*/
