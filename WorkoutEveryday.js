1a31e82d71f2e6cdeff1714dff2211b47ddef5e3
LishaDeng <lishadeng@hotmail.com>
2016-11-17 22:07:00 -0500

    add menu

--- WorkoutEveryday.js
+++ WorkoutEveryday.js
@@ -34,11 +34,11 @@ setTimeout(function(){
 
 
 function openNav() {
-   document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-   document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
 
 
